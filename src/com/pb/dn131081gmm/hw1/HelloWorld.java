81c251781a332bc5b854bc4ffaad7254c5eea336
Geresimik Mikhail <mikhail.gerasimik@privatbank.ua>
2021-10-11 13:51:23 +0300

    init commit

--- src/com/pb/dn131081gmm/hw1/HelloWorld.java
+++ src/com/pb/dn131081gmm/hw1/HelloWorld.java
@@ -2,6 +2,6 @@ package com.pb.dn131081gmm.hw1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
     }
 }
